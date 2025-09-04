b7ac17135511e4b3a2a69593ff52bf162286d577
Srishti Srivastava <146706947+SrishtiSrivastava23@users.noreply.github.com>
2025-09-04 19:05:59 +0530

    Create pattern2.cpp

---
+++ pattern2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void Pyra(int n)
+{
+    for(int i=n;i>0;i--)
+    {
+        for(int k=i;k>0;k--)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+int n=5;
+Pyra(n);
+return 0;
+}
