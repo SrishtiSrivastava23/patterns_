27473e50d9c268e3922ca0ef260f5e91b0566ae3
Srishti Srivastava <146706947+SrishtiSrivastava23@users.noreply.github.com>
2025-09-04 18:50:06 +0530

    Update and rename patter_1.cpp to pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void Pyra(int n)
+{
+    for(int i=1;i<n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+int n=5;
+Pyra(n);
+return 0;
+}
